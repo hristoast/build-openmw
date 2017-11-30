9983d605ba420f6c276415b20333c487282f9689
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2017-11-29 18:43:35 -0600

    Several improvements
    * Added support for more distros, testing is needed.
    * Added a --verbose argument that enables showing output from stuff.
    * The time is shown at start and end, and the total duration is given
      upon completion.

--- build-openmw.py
+++ build-openmw.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import datetime
 import logging
 import os
 import shutil
@@ -13,14 +14,17 @@ UNSHIELD_VERSION = "1.4.2"
 
 CPUS = os.cpu_count() + 1
 INSTALL_PREFIX = os.path.join("/", "opt", "morrowind")
-DESC = "Build OpenMW for your system, install it all to {}.  Also builds FFMpeg, OSG, and libBullet, and links against those builds.".format(INSTALL_PREFIX)
+DESC = "Build OpenMW for your system, install it all to {}.  Also builds OSG, libBullet, Unshield, and MyGUI, and links against those builds.".format(INSTALL_PREFIX)
 LOGFMT = '|-> %(message)s'
-SRC_DIR = os.path.join(os.getenv("HOME"), "src")
+OUT_DIR = os.getenv("HOME")
+SRC_DIR = os.path.join(INSTALL_PREFIX, "src")
 
-DEBIAN_PKGS = "TODO"
-UBUNTU_PKGS = "TODO"
-VOID_PKGS = "boost-devel cmake freetype-devel gcc git libmygui-devel libopenal-devel libopenjpeg2-devel libtxc_dxtn libunshield-devel liblzma-devel libXt-devel make nasm ois-devel python-devel python3-devel qt-devel SDL2-devel zlib-devel".split()
+DEBIAN_PKGS = "git libopenal-dev libsdl2-dev libqt4-dev libboost-filesystem-dev libboost-thread-dev libboost-program-options-dev libboost-system-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev libbullet-dev libmygui-dev libunshield-dev cmake build-essential libqt4-opengl-dev".split()
+REDHAT_PKGS = "openal-devel SDL2-devel qt4-devel boost-filesystem git boost-thread boost-program-options boost-system ffmpeg-devel ffmpeg-libs bullet-devel gcc-c++ mygui-devel unshield-devel tinyxml-devel cmake".split()
+UBUNTU_PKGS = DEBIAN_PKGS
+VOID_PKGS = "boost-devel cmake ffmpeg-devel freetype-devel gcc git libavformat libavutil libmygui-devel libopenal-devel libopenjpeg2-devel libswresample libswscale libtxc_dxtn liblzma-devel libXt-devel make nasm ois-devel python-devel python3-devel qt-devel SDL2-devel zlib-devel".split()
 
+PROG = 'build-openmw'
 VERSION = "1.1"
 
 
@@ -64,10 +68,13 @@ def error_and_die(msg: str) -> SystemExit:
     sys.exit(1)
 
 
-def execute_shell(cli_args: list, env=None) -> tuple:
+def execute_shell(cli_args: list, env=None, verbose=False) -> tuple:
     """Small convenience wrapper around subprocess.Popen."""
-    p = subprocess.Popen(cli_args, stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE, env=env)
+    if verbose:
+        p = subprocess.Popen(cli_args, env=env)
+    else:
+        p = subprocess.Popen(cli_args, stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE, env=env)
     c = p.communicate()
     return p.returncode, c
 
@@ -75,7 +82,7 @@ def execute_shell(cli_args: list, env=None) -> tuple:
 def build_library(libname, check_file=None, clone_dest=None, cmake=True,
                   cmake_args=None, cpus=None, env=None,
                   force=False, install_prefix=INSTALL_PREFIX, git_url=None,
-                  quiet=False, src_dir=SRC_DIR, version='master'):
+                  quiet=False, src_dir=SRC_DIR, verbose=False, version='master'):
 
     def _cmake():
         emit_log("{} building with cmake!".format(libname))
@@ -91,19 +98,21 @@ def build_library(libname, check_file=None, clone_dest=None, cmake=True,
         if cmake_args:
             build_cmd += cmake_args
         build_cmd += ["..", ]
-        exitcode, output = execute_shell(build_cmd, env=env)
+        exitcode, output = execute_shell(build_cmd, env=env, verbose=verbose)
         if exitcode != 0:
             emit_log(output[1])
             error_and_die("cmake exited nonzero!")
 
         emit_log("{} running make (this will take a while) ...".format(libname))
-        exitcode, output = execute_shell(["make", "-j{}".format(cpus)], env=env)
+        exitcode, output = execute_shell(["make", "-j{}".format(cpus)],
+                                         env=env, verbose=verbose)
         if exitcode != 0:
             emit_log(output[1])
             error_and_die("make exited nonzero!")
 
         emit_log("{} running make install ...".format(libname))
-        out, err = execute_shell(["make", "install"], env=env)[1]
+        out, err = execute_shell(["make", "install"],
+                                 env=env, verbose=verbose)[1]
         if err:
             error_and_die(err.decode("utf-8"))
 
@@ -113,25 +122,27 @@ def build_library(libname, check_file=None, clone_dest=None, cmake=True,
         emit_log("{} building with configure and make!".format(libname))
 
         emit_log("{} running make clean ...".format(libname))
-        out, err = execute_shell(["make", "clean"])[1]
+        out, err = execute_shell(["make", "clean"], verbose=verbose)[1]
         # if err:
         #     error_and_die(err.decode("utf-8"))
 
         emit_log("{} running configure ...".format(libname))
         out, err = execute_shell(["./configure",
                                   "--prefix={0}/{1}".format(install_prefix,
-                                                            libname)])[1]
+                                                            libname)],
+                                 verbose=verbose)[1]
         if err:
             error_and_die(err.decode("utf-8"))
 
         emit_log("{} running make (this will take a while) ...".format(libname))
-        exitcode, output = execute_shell(["make", "-j{}".format(cpus)])
+        exitcode, output = execute_shell(["make", "-j{}".format(cpus)],
+                                         verbose=verbose)
         if exitcode != 0:
             emit_log(output[1])
             error_and_die("make exited nonzero!")
 
         emit_log("{} running make install ...".format(libname))
-        out, err = execute_shell(["make", "install"])[1]
+        out, err = execute_shell(["make", "install"], verbose=verbose)[1]
         if err:
             error_and_die(err.decode("utf-8"))
 
@@ -140,21 +151,24 @@ def build_library(libname, check_file=None, clone_dest=None, cmake=True,
     def _git_clean_src():
         os.chdir(os.path.join(src_dir, clone_dest))
         emit_log("{} executing source clean!".format(libname))
-        execute_shell(["git", "checkout", "--", "."])
-        execute_shell(["git", "clean", "-df"])
-        emit_log("{} resetting source to the desired rev ({rev})".format(libname, rev=version))
-        execute_shell(["git", "checkout", version])
-        execute_shell(["git", "reset", "--hard", "origin/{}".format(version)])
+        execute_shell(["git", "checkout", "--", "."], verbose=verbose)
+        execute_shell(["git", "clean", "-df"], verbose=verbose)
+        emit_log("{} resetting source to the desired rev ({rev})".format(
+            libname, rev=version))
+        execute_shell(["git", "checkout", version], verbose=verbose)
+        execute_shell(["git", "reset", "--hard", "origin/{}".format(version)],
+                      verbose=verbose)
 
     if not clone_dest:
         clone_dest = libname
     if os.path.exists(check_file) and os.path.isfile(check_file) and not force:
         emit_log("{} found!".format(libname))
     else:
+        emit_log("{} building now ...".format(libname))
         if not os.path.exists(os.path.join(src_dir, clone_dest)):
             emit_log("{} source directory not found, cloning...".format(clone_dest))
             os.chdir(src_dir)
-            execute_shell(["git", "clone", git_url, clone_dest])[1]
+            execute_shell(["git", "clone", git_url, clone_dest], verbose=verbose)[1]
             if not os.path.exists(os.path.join(src_dir, libname)):
                 error_and_die("Could not clone {} for some reason!".format(clone_dest))
 
@@ -177,7 +191,10 @@ def get_distro() -> tuple:
 
 
 def get_openmw_sha(src_dir: str, version: str, pull=True) -> str:
-    os.chdir(os.path.join(src_dir, "openmw"))
+    try:
+        os.chdir(os.path.join(src_dir, "openmw"))
+    except FileNotFoundError:
+        return False
     if pull:
         emit_log("Fetching latest sources ...")
         execute_shell(["git", "fetch", "--all"])[1][0]
@@ -200,13 +217,35 @@ def install_packages(distro: str, **kwargs) -> bool:
     emit_log("Attempting to install dependency packages, please enter your sudo password as needed...",
              quiet=quiet)
     if 'void' in distro.lower():
+        emit_log("Distro detected as 'Void Linux'!")
         cmd = ["sudo", "xbps-install", "--yes"] + VOID_PKGS
         out, err = execute_shell(cmd)[1]
         msg = "Package installation completed!"
-    # elif 'debian' in distro.lower():
-    #     error_and_die("NYI!")
-    # elif 'ubuntu' in distro.lower():
-    #     error_and_die("NYI!")
+    elif 'debian' in distro.lower():
+        emit_log("Distro detected as 'Debian'!")
+        cmd = ["sudo", "apt-get", "install", "-y", "--allow-downgrades",
+               "--allow-remove-essential", "--allow-change-held-packages"] + DEBIAN_PKGS
+        out, err = execute_shell(cmd)[1]
+        msg = "Package installation completed!"
+    elif 'devuan' in distro.lower():
+        emit_log("Distro detected as 'Devuan'!")
+        # Debian packages should just work in this case.
+        cmd = ["sudo", "apt-get", "install", "-y", "--force-yes"] + DEBIAN_PKGS
+        out, err = execute_shell(cmd)[1]
+        msg = "Package installation completed!"
+    elif 'ubuntu' in distro.lower():
+        emit_log("Distro detected as 'Ubuntu'!")
+        cmd = ["sudo", "apt-get", "install", "-y", "--allow-downgrades",
+               "--allow-remove-essential", "--allow-change-held-packages"] + UBUNTU_PKGS
+        out, err = execute_shell(cmd)[1]
+        msg = "Package installation completed!"
+    elif 'fedora' in distro.lower():
+        emit_log("Distro detected as 'Fedora'!")
+        cmd = ["sudo", "dnf", "groupinstall", "-y", "development-tools"]
+        out, err = execute_shell(cmd)[1]
+        cmd = ["sudo", "dnf", "install", "-y"] + REDHAT_PKGS
+        out, err = execute_shell(cmd)[1]
+        msg = "Package installation completed!"
     else:
         error_and_die("Your OS is not yet supported!  If you think you know what you are doing, you can use '-S' to continue anyways.")
 
@@ -216,7 +255,7 @@ def install_packages(distro: str, **kwargs) -> bool:
 
 def parse_argv(_argv: list) -> None:
     """Set up args ('_argv', a list) and parse them."""
-    parser = argparse.ArgumentParser(description=DESC, prog='build-openmw')
+    parser = argparse.ArgumentParser(description=DESC, prog=PROG)
     parser.add_argument("--version", action="version", version=VERSION, help=argparse.SUPPRESS)
     options = parser.add_argument_group("Options")
     options.add_argument("--force-bullet", action="store_true", help="Force build LibBullet.")
@@ -237,17 +276,22 @@ def parse_argv(_argv: list) -> None:
                          help="Don't do a 'git fetch --all' on the OpenMW sources.")
     options.add_argument("-n", "--no-pkg", "--no-tar", action="store_true",
                          help="Don't create a package.")
+    options.add_argument("-o", "--out", metavar="DIR",
+                         help="Where to write the package to.  Default: {}".format(OUT_DIR))
     options.add_argument("-S", "--skip-install-pkgs", action="store_true",
                          help="Don't try to install dependencies.")
     options.add_argument("--src-dir", help="Set the source directory. Default: {}".format(SRC_DIR))
-    # TODO: verbose option that allows showing build/make output
+    options.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output.")
 
     return parser.parse_args()
 
 
 def main() -> None:
+    # TODO: option to skip a given dependency?
     logging.basicConfig(format=LOGFMT, level=logging.INFO, stream=sys.stdout)
-
+    start = datetime.datetime.now()
+    emit_log("BEGIN {0} run at {1}".format(
+        PROG, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     cpus = CPUS
     distro = None
     force_bullet = False
@@ -259,11 +303,13 @@ def main() -> None:
     install_prefix = INSTALL_PREFIX
     parsed = parse_argv(sys.argv[1:])
     rev = "master"
-    # just_openmw = False
-    # no_pkg = False
+    just_openmw = False
+    no_pkg = False
+    out_dir = OUT_DIR
     pull = True
     skip_install_pkgs = False
     src_dir = SRC_DIR
+    verbose = False
 
     if parsed.force_all:
         force_bullet = True
@@ -300,34 +346,45 @@ def main() -> None:
     if parsed.jobs:
         cpus = parsed.jobs
         emit_log("'-j{}' will be used with make!".format(cpus))
-    # if parsed.just_openmw:
-    #     just_openmw = parsed.just_openmw
-    #     emit_log("Just OpenMW enabled!")
-    # if parsed.no_pkg:
-    #     no_pkg = parsed.no_pkg
-    #     emit_log("No package will be made!")
+    if parsed.just_openmw:
+        just_openmw = parsed.just_openmw
+        emit_log(just_openmw, level=logging.DEBUG)  # TODO: remove this
+        emit_log("Just OpenMW enabled!")
+    if parsed.no_pkg:
+        no_pkg = parsed.no_pkg
+        emit_log(no_pkg, level=logging.DEBUG)  # TODO: remove this
+        emit_log("No package will be made!")
     if parsed.no_pull:
         pull = False
         emit_log("git fetch will not be ran!")
+    if parsed.out:
+        out_dir = parsed.out
+        emit_log("Out dir set to: {}".format(out_dir))
     if parsed.skip_install_pkgs:
         skip_install_pkgs = parsed.skip_install_pkgs
         emit_log("Package installs will be skipped!")
     if parsed.src_dir:
         src_dir = parsed.src_dir
         emit_log("Source directory set to: {}".format(src_dir))
+    if parsed.verbose:
+        verbose = parsed.verbose
+        emit_log("Verbose output enabled!")
 
     openmw_sha = get_openmw_sha(src_dir, rev, pull=pull)
 
-    out, err = get_distro()
+    try:
+        out, err = get_distro()
+    except FileNotFoundError:
+        error_and_die("Unable to determine your distro to install dependencies!  Try again and use '-S' if you know what you are doing.")
     if err:
-        error_and_die(err)
+        error_and_die(err.decode())
     else:
         distro = out.decode().split(":")[1].strip()
 
     if not skip_install_pkgs:
         out, err = install_packages(distro)
         if err:
-            error_and_die(err)
+            error_and_die(err.decode())
 
     ensure_dir(install_prefix)
     ensure_dir(src_dir)
@@ -341,16 +398,22 @@ def main() -> None:
                   git_url='https://github.com/FFmpeg/FFmpeg.git',
                   install_prefix=install_prefix,
                   src_dir=src_dir,
+                  verbose=verbose,
                   version=FFMPEG_VERSION)
 
     # OSG-OPENMW
     build_library("osg-openmw",
                   check_file=os.path.join(install_prefix, "osg-openmw", "lib64", "libosg.so"),
+                  cmake_args=["-DBUILD_OSG_PLUGINS_BY_DEFAULT=0", "-DBUILD_OSG_PLUGIN_OSG=1",
+                              "-DBUILD_OSG_PLUGIN_DDS=1", "-DBUILD_OSG_PLUGIN_TGA=1",
+                              "-DBUILD_OSG_PLUGIN_BMP=1", "-DBUILD_OSG_PLUGIN_JPEG=1",
+                              "-DBUILD_OSG_PLUGIN_PNG=1", "-DBUILD_OSG_DEPRECATED_SERIALIZERS=0"],
                   cpus=cpus,
                   force=force_osg,
                   git_url='https://github.com/OpenMW/osg.git',
                   install_prefix=install_prefix,
-                  src_dir=src_dir)
+                  src_dir=src_dir,
+                  verbose=verbose)
 
     # BULLET
     build_library("bullet",
@@ -363,6 +426,7 @@ def main() -> None:
                   git_url='https://github.com/bulletphysics/bullet3.git',
                   install_prefix=install_prefix,
                   src_dir=src_dir,
+                  verbose=verbose,
                   version=BULLET_VERSION)
 
     # UNSHIELD
@@ -373,6 +437,7 @@ def main() -> None:
                   git_url='https://github.com/twogood/unshield.git',
                   install_prefix=install_prefix,
                   src_dir=src_dir,
+                  verbose=verbose,
                   version=UNSHIELD_VERSION)
 
     # MYGUI
@@ -385,15 +450,21 @@ def main() -> None:
                   git_url='https://github.com/MyGUI/mygui.git',
                   install_prefix=install_prefix,
                   src_dir=src_dir,
+                  verbose=verbose,
                   version=MYGUI_VERSION)
 
     # OPENMW
+    if openmw_sha:
+        openmw = "openmw-{}".format(openmw_sha)
+    else:
+        # There's no sha yet since the source hasn't been cloned.
+        openmw = "openmw"
     build_env = os.environ.copy()
     build_env["CMAKE_PREFIX_PATH"] = "{0}/ffmpeg:{0}/osg-openmw:{0}/unshield:{0}/mygui:{0}/bullet".format(
         install_prefix)
     build_env["LDFLAGS"] = "-llzma -lz -lbz2"
-    build_library("openmw-{}".format(openmw_sha),
-                  check_file=os.path.join(install_prefix, "openmw-{}".format(openmw_sha), "bin", "openmw"),
+    build_library(openmw,
+                  check_file=os.path.join(install_prefix, openmw, "bin", "openmw"),
                   clone_dest="openmw",
                   cpus=cpus,
                   env=build_env,
@@ -401,13 +472,28 @@ def main() -> None:
                   git_url='https://github.com/OpenMW/openmw.git',
                   install_prefix=install_prefix,
                   src_dir=src_dir,
+                  verbose=verbose,
                   version=rev)
     os.chdir(install_prefix)
+    # Don't fetch updates since new ones might exist
+    openmw_sha = get_openmw_sha(src_dir, rev, pull=False)
+    if str(openmw_sha) not in openmw:
+        os.chdir(install_prefix)
+        os.rename("openmw", "openmw-{}".format(openmw_sha))
     if os.path.islink("openmw"):
         os.remove("openmw")
     os.symlink("openmw-{}".format(openmw_sha), "openmw")
 
     # TODO: Create tar file
+    # ⌜
+    # ∟
+    emit_log("END {0} run at {1}".format(
+        PROG, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    end = datetime.datetime.now()
+    duration = end - start
+    minutes = int(duration.total_seconds() // 60)
+    seconds = int(duration.total_seconds() % 60)
+    emit_log("Took {m} minutes, {s} seconds.".format(m=minutes, s=seconds))
 
 
 if __name__ == '__main__':
