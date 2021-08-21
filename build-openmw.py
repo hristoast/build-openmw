24148f18b07f5bd8f44e1d94a2f6e2094a40ae4c
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2021-08-21 17:04:57 -0500

    That arg was wrong

--- build-openmw.py
+++ build-openmw.py
@@ -764,7 +764,7 @@ def main() -> None:
                 "-DINSTALL_LIBS=on",
                 "-DBUILD_SHARED_LIBS=on",
                 "-DBULLET2_MULTITHREADING=on",
-                "-DUSE_DOUBLE_PRECISION=on",
+                "-DBT_USE_DOUBLE_PRECISION=on",
             ],
             cpus=cpus,
             force=force_bullet,
